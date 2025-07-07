83e5422ea4e992c6913569b969f0e00fc540961a
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-07 23:43:53 +0545

    Ex: 4.19 & 4.20 Middleware to Token Extraction

--- part-4/bloglist/utils/middleware.js
+++ part-4/bloglist/utils/middleware.js
@@ -1,5 +1,15 @@
 // utils/middleware.js
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
 //   console.error(error.name, error.message)
 
@@ -16,4 +26,5 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   errorHandler,
+  tokenExtractor
 }
