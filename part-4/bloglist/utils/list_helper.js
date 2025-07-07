66387a4ed3476939e7f2919296431f53e9406234
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-07 18:34:16 +0545

    EX:4.6 add mostBlogs func

--- part-4/bloglist/utils/list_helper.js
+++ part-4/bloglist/utils/list_helper.js
@@ -6,17 +6,50 @@ const dummy = (blogs) => {
   return 1;
 }
 
+//for totalLikes function
+// This function calculates the total number of likes across all blogs
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 }
 
+//for favoriteBlog function
+// This function finds the blog with the most likes
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null
   return blogs.reduce((fav, blog) => (blog.likes > (fav.likes || 0) ? blog : fav), {})
 }
 
+//for mostBlogs function 
+// This function finds the author with the most blogs
+// It returns an object with the author's name and the number of blogs they have written
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const counts = {}
+
+  blogs.forEach(blog => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+  })
+
+  let maxAuthor = null
+  let maxBlogs = 0
+
+  for (const author in counts) {
+    if (counts[author] > maxBlogs) {
+      maxBlogs = counts[author]
+      maxAuthor = author
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    blogs: maxBlogs
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
