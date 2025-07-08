16af4c068efbe267741da86efb8e3437d3288dd2
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-08 09:02:52 +0545

    Add dotenv

--- part-4/bloglist/app.js
+++ part-4/bloglist/app.js
@@ -5,12 +5,14 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+require('dotenv').config()
 
-
+// const config = require('./utils/config')
+// const logger = require('./utils/logger')
 
 const app = express()
 
-const mongoUrl = 'mongodb+srv://tolangeakash753:bloglist@cluster1.go3atd8.mongodb.net/BlogList?retryWrites=true&w=majority&appName=Cluster1'
+// const mongoUrl = 'mongodb+srv://tolangeakash753:bloglist@cluster1.go3atd8.mongodb.net/BlogList?retryWrites=true&w=majority&appName=Cluster1'
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
@@ -18,6 +20,7 @@ app.use('/api/blogs', blogsRouter)
 app.use(middleware.errorHandler)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)  
-app.use(middleware.tokenExtractor)
+// app.use(middleware.tokenExtractor)
+app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor,blogsRouter)
 
 module.exports = app
