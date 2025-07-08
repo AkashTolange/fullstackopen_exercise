8a43fadfdbe8baf1b599d82e983fb01209e7c5ce
Akash Tolange <akashhtolangee77@gmail.com>
2025-07-08 09:06:15 +0545

    Ex: 4.23 completed

--- part-4/bloglist/tests/blog_api.test.js
+++ part-4/bloglist/tests/blog_api.test.js
@@ -8,9 +8,15 @@ const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
 
+//import this 
+const bcrypt = require('bcrypt')  
+const User = require('../models/user')
+
 //supertest is used to test HTTP requests
 //supertest(app) creates a test instance of the app
 const api = supertest(app)
+
+
 
 const initialBlogs = [
   {
@@ -27,11 +33,25 @@ const initialBlogs = [
   },
 ]
 
+
+let token =''
+
 // Connect to the test database before running tests
 //beforeEach is used to run a function before each test
 beforeEach(async () => {
   await Blog.deleteMany({})
   await Blog.insertMany(initialBlogs)
+
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+  await user.save()
+
+  const result = await api
+    .post('/api/login')
+    .send({ username: 'root', password: 'sekret' })
+
+  token = result.body.token
+
 })
 
 test('blogs are returned as JSON and correct length', async () => {
@@ -57,23 +77,24 @@ test('blog posts have id property defined', async () => {
 
 test('a valid blog can be added', async () => {
   const newBlog = {
-    title: 'New Blog',
-    author: 'New Author',
-    url: 'http://newblog.com',
+    title: 'Token Auth Blog',
+    author: 'Akash',
+    url: 'http://akashblog.com',
     likes: 10,
   }
 
   await api
     .post('/api/blogs')
+    .set('Authorization', `Bearer ${token}`) // Set the token in the header
     .send(newBlog)
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
   const blogsAtEnd = await Blog.find({})
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+  expect(blogsAtEnd).toHaveLength(1)
 
-  const titles = blogsAtEnd.map(b => b.title)
-  expect(titles).toContain('New Blog')
+  // const titles = blogsAtEnd.map(b => b.title)
+  // expect(titles).toContain('New Blog')
 })
 
 
@@ -145,6 +166,50 @@ test('a blog\'s likes can be updated', async () => {
     .expect('Content-Type', /application\/json/)
 
   expect(response.body.likes).toBe(blogToUpdate.likes + 5)
+})
+
+//adding failing test if no token 
+test('blog post fails without token', async () => {
+  const newBlog = {
+    title: 'No Token',
+    author: 'Anonymous',
+    url: 'https://example.com',
+    likes: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(401)
+
+  const blogs = await Blog.find({})
+  expect(blogs).toHaveLength(0)
+})
+
+
+//adding test DELETE only works if user owns the blog 
+test('blog can be deleted by the user who created it', async () => {
+  const newBlog = {
+    title: 'Deletable Blog',
+    author: 'Owner',
+    url: 'https://delete.com',
+    likes: 1
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .set('Authorization', `Bearer ${token}`)
+    .send(newBlog)
+
+  const blogToDelete = response.body
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .set('Authorization', `Bearer ${token}`)
+    .expect(204)
+
+  const blogsAfter = await Blog.find({})
+  expect(blogsAfter).toHaveLength(0)
 })
 
 
