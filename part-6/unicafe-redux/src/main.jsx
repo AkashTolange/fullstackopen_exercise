90f81df8c591313cedb3a215ca8ade8014cf7fb4
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-27 08:49:26 +0545

    feat: Ex: 6.1 implement Redux reducer and store for feedback (good, ok, bad)

---
+++ part-6/unicafe-redux/src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { createStore } from 'redux'
+// import reducer from './reducer'
+import counterReducer from './reducer'
+
+const store = createStore(counterReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const App =() => { 
+  return ( 
+    <div>
+      <h2>Give Feedback ok </h2>
+      {/* <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>reset stats</button> */}
+
+      <h2>Statistics</h2>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+
+
+
+
+
+const renderApp = () => {
+  root.render(<App/>)
+  
+}
+
+renderApp()
+store.subscribe(renderApp)
