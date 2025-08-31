2224735b09ccf9ffc396eeba42759a2f12cd7b75
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-31 22:40:15 +0545

    Add: apply filter and sort by votes

--- part-6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,19 +2,22 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)  // get anecdotes from Redux
   const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
 
   const handleVote = (id) => {
-    dispatch(voteAnecdote(id))  // send vote action
+    dispatch(voteAnecdote(id))
   }
 
-  // Sort anecdotes by votes (descending)
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  // Apply filter and sort by votes
+  const filteredAnecdotes = [...anecdotes]
+    .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+    .sort((a, b) => b.votes - a.votes)
 
   return (
     <div>
-      {sortedAnecdotes.map(anecdote =>
+      {filteredAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
