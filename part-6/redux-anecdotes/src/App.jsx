addc939a5c90ae01b05b4025331d0582476c24c8
Akash Tolange <akashhtolangee77@gmail.com>
2025-08-31 22:22:40 +0545

    Add: move all logic realated to voting to this new component AnecdoteList & called in App

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -1,35 +1,36 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
-import { useRef } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
+// import { useRef } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  // const anecdotes = useSelector(state => state)
+  // const dispatch = useDispatch()
   //told yeah 
-  const inputRef = useRef()
+  // const inputRef = useRef()
 
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(voteAnecdote(id))
-  }
+  // const vote = (id) => {
+  //   console.log('vote', id)
+  //   dispatch(voteAnecdote(id))
+  // }
 
   //ex:6.4
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = inputRef.current.value
-    if (content) {
-      dispatch(createAnecdote(content))
-      inputRef.current.value = '' // clear input
-    }
-  }
+  // const addAnecdote = (event) => {
+  //   event.preventDefault()
+  //   const content = inputRef.current.value
+  //   if (content) {
+  //     dispatch(createAnecdote(content))
+  //     inputRef.current.value = '' // clear input
+  //   }
+  // }
 
   return (
     <div>
       <h2>Anecdotes</h2>
 
       {/* to make anecdotes ordered by number of votes, modify the rendering in this  */}
-      {[...anecdotes]
+      {/* {[...anecdotes]
       .sort((a,b) => b.votes - a.votes)         //sort descednign by votes
       .map(anecdote =>
         <div key={anecdote.id}>
@@ -41,7 +42,8 @@ const App = () => {
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      )} */}
+      <AnecdoteList/>
       {/* from here */}
       {/* <h2>create new</h2>
       <form onSubmit={addAnecdote}>
---
+++ part-6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)  // get anecdotes from Redux
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    dispatch(voteAnecdote(id))  // send vote action
+  }
+
+  // Sort anecdotes by votes (descending)
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+
+  return (
+    <div>
+      {sortedAnecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
