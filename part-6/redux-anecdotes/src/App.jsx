8dac84d480e07d2eac87940763a4c0e989e2ea13
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-02 08:48:32 +0545

    Add: Ex: 6.14–6.15: integrate backend with anecdotes (fetch & create) && feat: fetch anecdotes from backend and store new anecdotes in server

--- part-6/redux-anecdotes/src/App.jsx
+++ part-6/redux-anecdotes/src/App.jsx
@@ -5,6 +5,11 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+
+
 const App = () => {
   // const anecdotes = useSelector(state => state)
   // const dispatch = useDispatch()
@@ -25,6 +30,13 @@ const App = () => {
   //     inputRef.current.value = '' // clear input
   //   }
   // }
+
+  //for 6.14. to 15 part
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
 
   return (
     <div>
--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,47 +1,48 @@
+// reducers/anecdoteReducer.js
 import { createSlice } from '@reduxjs/toolkit'
-
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => ({
-  content: anecdote,
-  id: getId(),
-  votes: 0
-})
-
-const initialState = anecdotesAtStart.map(asObject)
+import anecdoteService from '../services/anecdotes'  // we'll create this service file
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
+    // set anecdotes when initializing
+    setAnecdotes(state, action) {
+      return action.payload
+    },
+    // add a new anecdote
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    // vote for an anecdote
     voteAnecdote(state, action) {
       const id = action.payload
       const anecdoteToVote = state.find(a => a.id === id)
       if (anecdoteToVote) {
-        anecdoteToVote.votes += 1  // Redux Toolkit allows direct mutation
+        anecdoteToVote.votes += 1
       }
-    },
-    createAnecdote(state, action) {
-      state.push({
-        content: action.payload,
-        id: getId(),
-        votes: 0
-      })
     }
   }
 })
 
-// Export action creators
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export const { setAnecdotes, appendAnecdote, voteAnecdote } = anecdoteSlice.actions
 
-// Export reducer to use in store
+// === Thunks ===
+
+// fetch anecdotes from backend
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+// create new anecdote in backend
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
 export default anecdoteSlice.reducer
