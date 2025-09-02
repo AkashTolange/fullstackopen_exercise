6435748504295a93318538ea89b0dd1b8c0886f5
Akash Tolange <akashhtolangee77@gmail.com>
2025-09-02 09:20:39 +0545

    feat: improved notification action creator and slice with a single thunk

--- part-6/redux-anecdotes/src/components/Notification.js
+++ part-6/redux-anecdotes/src/components/Notification.js
@@ -1,14 +1,16 @@
+import { useSelector } from 'react-redux'
+
+const style = {
+  border: 'solid',
+  padding: 10,
+  borderWidth: 1,
+  marginBottom: 10,
+}
+
 const Notification = () => {
-  const style = {
-    border: 'solid',
-    padding: 10,
-    borderWidth: 1
-  }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
+  const message = useSelector(state => state.notification)
+  if (!message) return null
+  return <div style={style}>{message}</div>
 }
 
 export default Notification
